2cd089b22ce11e16175c4e7bf063938c89935fa7
leecheonbae <dlcjsqo999@naver.com>
2016-04-20 16:00:08 +0900

    .

--- test01/src/test02.java
+++ test01/src/test02.java
@@ -3,6 +3,9 @@ public class test02 {
 
   public static void main(String[] args) {
     System.out.println("hello");
+    
+    System.out.println("okok");
+    System.out.println("nono");
   }
 
 }
